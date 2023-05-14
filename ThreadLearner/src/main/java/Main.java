72d22eca6dad47a099f63454f9fb8592fb321722
RlSk2112 <lrm1201@abv.bg>
2023-05-14 21:50:28 +0300

    Running two threads at the same time and printing a message after finished

--- ThreadLearner/src/main/java/Main.java
+++ ThreadLearner/src/main/java/Main.java
@@ -1,20 +1,15 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
+
         ThreadOne threadOne = new ThreadOne();
+        ThreadTwo threadTwo = new ThreadTwo();
         threadOne.start();
+        threadTwo.start();
 
-        int index = 0;
-        while (index < 10) {
+        threadOne.join();
+        threadTwo.join();
+        System.out.println("End");
 
-            System.out.printf("Index %d from thread: %s%n" , index, Thread.currentThread().getName());
-
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-
-            index++;
-        }
     }
+
 }
--- ThreadLearner/src/main/java/ThreadTwo.java
+++ ThreadLearner/src/main/java/ThreadTwo.java
@@ -1,16 +1,16 @@
-class ThreadOne extends Thread {
+public class ThreadTwo extends Thread {
+
 
     @Override
     public void run() {
 
         for (int i = 0; i < 10; i++) {
             try {
-                Thread.sleep(1000);
+                Thread.sleep(2000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
             System.out.printf("%s%n", this.getName());
         }
     }
-
 }
